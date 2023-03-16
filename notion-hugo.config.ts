0e078eb4ccc8db44f9b70ad53845d2b5e588bcb8
magicgh <magicgher@hotmail.com>
2023-03-16 20:25:44 +0800

    :wrench: Add personal config

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: process.env.NOTION_PAGE_URL,
         pages: [
             // {
             //     page_id: '<page_id>',
