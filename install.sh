f5c5560395ff8dfbd888a1c12dc7952807f9619a
magicgh <magicgher@hotmail.com>
2023-03-22 01:03:09 +0800

    :fire: Remove layout.js

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cp -rf ../blog.config.js ./
-cp -rf ../layout.js ./layouts/
-cp -rf ../public/apple-touch-icon.png ../public/avatar.png ../public/favicon.cio ../public/favicon.svg ./public/
+cp -rf ./blog.config.js ./nobelium/
+cp -rf ./public/apple-touch-icon.png ./public/avatar.png ./public/favicon.cio ./public/favicon.svg ./nobelium/public/
