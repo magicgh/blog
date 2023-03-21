84c10b2a49df4b0e7f2a157b33781359842f8162
magicgh <magicgher@hotmail.com>
2023-03-22 01:04:17 +0800

    :pencil2: Fix typo in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -rf ./blog.config.js ./nobelium/
-cp -rf ./public/apple-touch-icon.png ./public/avatar.png ./public/favicon.cio ./public/favicon.svg ./nobelium/public/
+cp -rf ./public/apple-touch-icon.png ./public/avatar.png ./public/favicon.ico ./public/favicon.svg ./nobelium/public/
